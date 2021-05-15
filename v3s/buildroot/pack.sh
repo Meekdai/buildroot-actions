7eafb583e45747d294f004bbaa1d13c20ab37760
Meekdai <daijiayun@regenovo.com>
2021-05-15 18:16:33 +0800

    添加pack

--- v3s/buildroot/pack.sh
+++ v3s/buildroot/pack.sh
@@ -14,7 +14,7 @@ rm flash_32m.bin -rf
 cp output/images/inittab   output/target/etc/inittab
 
 ###添加一个启动脚本 (模拟时钟)###
-echo "/usr/bin/analogclock -platform linuxfb" > output/target/etc/profile.d/startup.sh
+# echo "/usr/bin/analogclock -platform linuxfb" > output/target/etc/profile.d/startup.sh
 
 ###wifi初始脚本###
 cp output/images/wpa_supplicant.conf output/target/etc/ -rf
@@ -35,12 +35,12 @@ dd if=/dev/zero bs=1M count=32 | tr "\000" "\377" > flash_32m.bin
 
 ###拷贝u-boot, dtb,kernel, filesystem###
 dd if=notrunc if=output/build/uboot-v3s-spi-experimental/u-boot-sunxi-with-spl.bin of=flash_32m.bin seek=0
-dd if=notrunc if=output/images/env.bin of=flash_32m.bin bs=$((0xF8000)) seek=1
+# dd if=notrunc if=output/images/env.bin of=flash_32m.bin bs=$((0xF8000)) seek=1
 dd if=notrunc if=output/images/sun8i-v3s-licheepi-zero-dock.dtb of=flash_32m.bin bs=$((0x100000)) seek=1
 dd if=notrunc if=output/images/zImage of=flash_32m.bin bs=$((0x110000)) seek=1
 dd if=notrunc if=jffs2.bin of=flash_32m.bin bs=$((0x610000)) seek=1
 
-tar czvf files.tgz jffs2.bin output/build/uboot-v3s-spi-experimental/u-boot-sunxi-with-spl.bin output/images/env.bin output/images/sun8i-v3s-licheepi-zero-dock.dtb output/images/zImage
+#tar czvf files.tgz jffs2.bin output/build/uboot-v3s-spi-experimental/u-boot-sunxi-with-spl.bin output/images/env.bin output/images/sun8i-v3s-licheepi-zero-dock.dtb output/images/zImage
 ###压缩###
-tar czvf flash.tgz flash_32m.bin
+# tar czvf flash.tgz flash_32m.bin
 
